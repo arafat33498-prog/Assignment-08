87a194ba6fe4df111b3fae872dff0f08a9fe5820
arafat33498-prog <arafat33598@gmail.com>
2026-05-01 17:02:37 +0600

    Fix: Add Google and production image patterns to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,14 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'i.postimg.cc',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com', // গুগলের প্রোফাইল ইমেজের জন্য
+      },
+      {
+        protocol: 'https',
+        hostname: 'assignment-08-sooty-phi.vercel.app', // আপনার লাইভ সাইটের ইমেজের জন্য
+      },
     ],
   },
 };
